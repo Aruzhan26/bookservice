6ec2959286fc97fec31ecc953e9342991befeaae
Aruzhan26 <40426904+Aruzhan26@users.noreply.github.com>
2019-11-11 15:29:32 +0600

    Hystrix Dashboard

--- book-catalog-service/src/main/java/com/example/bookcatalogservice/BookCatalogServiceApplication.java
+++ book-catalog-service/src/main/java/com/example/bookcatalogservice/BookCatalogServiceApplication.java
@@ -5,17 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
-//@EnableHystrixDashboard
+@EnableHystrixDashboard
 public class BookCatalogServiceApplication {
 
 	@Bean
